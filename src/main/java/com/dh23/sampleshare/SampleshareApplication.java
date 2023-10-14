99ed4cf251bdca991426f08fa8dc329d7a2542c2
keatonuw <keatonkowal@icloud.com>
2023-10-14 13:59:57 -0700

    spring initialer

---
+++ src/main/java/com/dh23/sampleshare/SampleshareApplication.java
@@ -0,0 +1,13 @@
+package com.dh23.sampleshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleshareApplication.class, args);
+	}
+
+}
